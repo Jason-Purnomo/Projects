7e7d9bd2078909ec95b999088b194210d92bde02
Jason-Purnomo <jasonpurnomo92@gmail.com>
2025-12-04 13:55:50 +0100

    Add files via upload
    Temperatursensor mit Sensor KTY81-210

---
+++ Temperatursensor2.ino
@@ -0,0 +1,47 @@
+int main(void){
+  init();
+  Serial.begin(9600);
+
+  //Konfiguration
+  //PINS SS, MOSI, MISO, SCK
+  DDRB |= (1 << DDB2) | (1 << DDB3) | (1 << DDB5);  
+  DDRB &= ~(1 << DDB4);      
+  //SPI
+  SPCR = (1 << MSTR) | (1 << SPE) | (1 << SPR0);
+  //Mode 0,0: SCLK idles low
+  //Daten werden bei steigender Flanke übernommen und gelesen
+
+  while(1){
+    //SS LOW setzen
+    PORTB &= ~(1 << PORTB2);
+
+    //SPI Transfer starten
+    SPDR = 0b00000001;          //Din
+    //auf Ende von SPI Transfer warten
+    while(!(SPSR & (1 << SPIF)));       //warten auf SPIF == 1
+    uint8_t Byte1 = SPDR;       //Dout
+
+
+    SPDR = 0b11100000;          //Din
+    //auf Ende von SPI Transfer warten
+    while(!(SPSR & (1 << SPIF)));       //warten auf SPIF == 1
+    uint8_t Byte2 = SPDR;       //Dout
+
+
+    SPDR = 0b00000000;          //Din
+    //auf Ende von SPI Transfer warten
+    while(!(SPSR & (1 << SPIF)));       //warten auf SPIF == 1
+    uint8_t Byte3 = SPDR;       //Dout
+
+    uint16_t Wert = ((Byte2 & 0x0F) << 8) | Byte3;        //0x0F = 0b00001111
+    float Voltage = (float)Wert * 5 / 4096;
+    float Rkty = 2200.0 * Voltage / (5.0 - Voltage);
+    float Temperatur = (Rkty - 1630.0)/14.8;             //Linearisierung
+
+    //SS HIGH setzen
+    PORTB |= (1 << PORTB2);
+
+    Serial.println(Temperatur);
+    _delay_ms(500);
+  }
+}
